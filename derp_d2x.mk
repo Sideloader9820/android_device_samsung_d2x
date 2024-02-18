58f2f2ba2d02fa7e641fea9974b0128938942efe
Sideloader9820 <160022449+Sideloader9820@users.noreply.github.com>
2024-02-18 09:54:46 +0000

    derp_d2x: adapt to derpfest

--- derp_d2x.mk
+++ derp_d2x.mk
@@ -36,14 +36,14 @@ TARGET_BOOTANIMATION_HALF_RES := true
 TARGET_SCREEN_HEIGHT := 3040
 TARGET_SCREEN_WIDTH := 1440
 
-## Inherit some common Lineage stuff
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+## Inherit some common Derpfest stuff
+$(call inherit-product, vendor/derp/config/common_full_phone.mk)
 
 ## Enable updating of APEXes
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
 ## Device identifier, this must come after all inclusions
-PRODUCT_NAME := lineage_d2x
+PRODUCT_NAME := derp_d2x
 PRODUCT_DEVICE := d2x
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := SM-N976B
