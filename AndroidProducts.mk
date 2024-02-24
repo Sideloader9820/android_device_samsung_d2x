26e04e4df060831d306544e4706f2bbf43a9852f
Sideloader9820 <160022449+Sideloader9820@users.noreply.github.com>
2024-02-24 15:57:48 +0000

    AndroidProducts: adapt to Rising

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_d2x.mk
+    $(LOCAL_DIR)/rising_d2x.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_d2x-eng \
-    lineage_d2x-user \
-    lineage_d2x-userdebug
+    rising_d2x-eng \
+    rising_d2x-user \
+    rising_d2x-userdebug
