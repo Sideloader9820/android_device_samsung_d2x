bb120989c128d9ea47552f5046724a9e864a7a26
Sideloader9820 <160022449+Sideloader9820@users.noreply.github.com>
2024-02-24 15:58:44 +0000

    rising_d2x: adapt to rising

--- rising_d2x.mk
+++ rising_d2x.mk
@@ -37,13 +37,13 @@ TARGET_SCREEN_HEIGHT := 3040
 TARGET_SCREEN_WIDTH := 1440
 
 ## Inherit some common Lineage stuff
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/rising/config/rising.mk)
 
 ## Enable updating of APEXes
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
 ## Device identifier, this must come after all inclusions
-PRODUCT_NAME := lineage_d2x
+PRODUCT_NAME := rising_d2x
 PRODUCT_DEVICE := d2x
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := SM-N976B
